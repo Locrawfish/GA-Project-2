6cd1a8a9338d47a693f447af5e1f2208c69a24ab
Loren Crawford <lmcrawf@gmail.com>
2015-09-17 12:34:05 -0400

    Added full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,12 @@
 module ApplicationHelper
+ # Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "TODO App"
+    base_title = "Residual Fare"
     if page_title.empty?
       base_title
     else
-      "#{base_title} | #{page_title}"
+      "#{page_title} | #{base_title}"
     end
   end
 end
+
