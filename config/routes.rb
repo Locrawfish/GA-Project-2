dc84f29a80bcf4e29ff9afbbd7ba3e773733a3e0
Loren Crawford <lmcrawf@gmail.com>
2015-09-17 11:40:15 -0400

    Adding titles to static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'static_pages#home'
+  root 'static_pages#home'
   match '/about', to: 'static_pages#about', via: 'get'
 
   # The priority is based upon order of creation: first created -> highest priority.
