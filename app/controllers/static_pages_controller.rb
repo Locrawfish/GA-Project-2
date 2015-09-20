7bec13806e0dc28f3e82916a70998b9e9e7211e8
Loren Crawford <lmcrawf@gmail.com>
2015-09-20 18:07:54 -0400

    Fixed a bug related to the user name in the nav bar

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,10 @@
 class StaticPagesController < ApplicationController
   before_action :logged_in_user, except: [:home, :about]
   def home
+    @user = current_user
   end
 
   def about
+    @user = current_user
   end
 end
